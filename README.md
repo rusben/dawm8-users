a6842bf308f4288e5eb6c418931f1cb4aa0aaf96
Rubén Arroyo <rusben@gmail.com>
2016-12-05 12:45:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#dawm8-users
+This is a schoolar project to show the access to a mysql database with php.
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$database = "dawm8-mysql-php";
+# Example (PDO)
+# Select Data With PDO (+ Prepared Statements)
+# The following example uses prepared statements.
+# We use PDO beacuse its portability
+# We use prepared statements, because its enhaced security
+# we use FETCH_ASSOC, theoretically faster because are basic types
+try {
+//    echo "----- Connection -----";
+//    echo "<br/>";
+    $conn = new PDO("mysql:host=$servername;dbname=$database",
+                        $username,
+                        $password,
+                        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  //  echo "----- SELECT all users -----";
+    $stmt = $conn->prepare("SELECT * FROM users");
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<table border="1">
+  <thead>
+    <th>id</th>
+    <th>email</th>
+    <th>name</th>
+    <th>surname</th>
+    <th>registered</th>
+    <th>lastLogin</th>
+    <th></th>
+  </thead>
+<?php foreach ($users as $key => $user) { ?>
+    <tr>
+      <td><?=$user['id']?></td>
+      <td><?=$user['email']?></td>
+      <td><?=$user['name']?></td>
+      <td><?=$user['surname']?></td>
+      <td><?=$user['registered']?></td>
+      <td><?=$user['lastLogin']?></td>
+      <td><a href="">delete</a> | <a href="">edit</a></td>
+    </tr>
+<?php } ?>
+</table>
+<?php
+    // Close connection
+    $db = null;
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
